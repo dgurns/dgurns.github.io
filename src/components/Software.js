d32959f6c9cbaba9cc5f9c1d5346322b7b869271
Dan Gurney <gurney.dan@gmail.com>
2018-08-20 18:20:19 -0400

    Update copy on Software. Add major brand project.

--- src/components/Software.js
+++ src/components/Software.js
@@ -7,17 +7,21 @@ export default function Software() {
   return (
     <div className="software">
       <div className="software__mainDescription">
-        Hello! I{"'"}m a software developer in New York{"'"}s beautiful Hudson
+        Hello! I{"'"}m a software engineer in New York{"'"}s beautiful Hudson
         Valley.
-        <br /><br />
-        I specialize in designing and building cross-platform apps. I often
-        work with React Native, React, and Meteor.
-        <br /><br />
-        I{"'"}m a co-founder of <a href="https://www.hudsonvalleyapps.com/">
-        Hudson Valley Apps.</a> Also: Founder of Concert Window and accordion
-        player.
-        <br /><br />
-        Get in touch at <a href="mailto:gurney.dan@gmail.com">info@dangurney.net</a>
+        <br />
+        <br />
+        I specialize in designing and building cross-platform apps. I often work
+        with React Native, React, and Meteor.
+        <br />
+        <br />
+        I{"'"}m a co-founder of{' '}
+        <a href="https://www.hudsonvalleyapps.com/">Hudson Valley Apps.</a>{' '}
+        Also: Founder of Concert Window and accordion player.
+        <br />
+        <br />
+        Get in touch at{' '}
+        <a href="mailto:gurney.dan@gmail.com">info@dangurney.net</a>
       </div>
 
       <div className="software__sectionTitle">
@@ -48,81 +52,72 @@ export default function Software() {
       <div className="software__process">
         <div className="software__processStep">
           <div className="software__processLabel">
-            <div className="software__processNumber">
-              1
-            </div>
+            <div className="software__processNumber">1</div>
           </div>
           <div className="software__processInfo">
             <span className="software__processInfo--title">Wireframing</span>
-            Every project is different, but once the strategy has been firmed up,
-            I usually begin by wireframing the core user interface. This includes
-            navigation, flows between views, and the impact of different
-            screen sizes. I like to do this on paper or with software
+            Every project is different, but once the strategy has been firmed
+            up, I usually begin by wireframing the core user interface. This
+            includes navigation, flows between views, and the impact of
+            different screen sizes. I like to do this on paper or with software
             like Balsamiq.
           </div>
         </div>
 
         <div className="software__processStep">
           <div className="software__processLabel">
-            <div className="software__processNumber">
-              2
-            </div>
+            <div className="software__processNumber">2</div>
           </div>
           <div className="software__processInfo">
             <span className="software__processInfo--title">Design</span>
-            For the design phase, I draw on my experience leading product at
-            Concert Window, a live broadcasting platform with 150,000 users. I
-            strive for simplicity, beauty, and intuitiveness. One thing I love to
-            do is creating interfaces for completely new activities; I also love
-            adapting and polishing. I usually do designs in Sketch.
+            Often I will use a client-supplied design, or in other cases I
+            handle the design phase myself and draw on my experience leading
+            product at Concert Window, a live broadcasting platform with 150,000
+            users. I strive for simplicity, beauty, and intuitiveness. One thing
+            I love to do is creating interfaces for completely new activities; I
+            also love adapting and polishing. I usually do designs in Sketch.
           </div>
         </div>
 
         <div className="software__processStep">
           <div className="software__processLabel">
-            <div className="software__processNumber">
-              3
-            </div>
+            <div className="software__processNumber">3</div>
           </div>
           <div className="software__processInfo">
             <span className="software__processInfo--title">Development</span>
-            This is the real meat of the process, and one of the benefits of
-            doing everything under one roof. The development phase joins a unified
-            flow with wireframing, design, testing, and launch, and I can revisit
-            the other phases as necessary. I usually build apps in React Native,
-            which enables cross-platform releases for iOS and Android smartphones
-            + tablets.
+            Whether I{"'"}m using a client-supplied design or one of my own, the
+            development phase is a true collaborative process where product
+            vision comes into focus with frequent alpha releases. I usually
+            build apps in React Native, which enables fully native
+            cross-platform releases for iOS and Android smartphones + tablets.
           </div>
         </div>
 
         <div className="software__processStep">
           <div className="software__processLabel">
-            <div className="software__processNumber">
-              4
-            </div>
+            <div className="software__processNumber">4</div>
           </div>
           <div className="software__processInfo">
             <span className="software__processInfo--title">Testing</span>
             Testing is when a good product becomes a great product. During this
             phase I iron out bugs, polish animations and interface, and conduct
             extensive cross-platform testing. I also like to do a beta program,
-            which can be good for validating pricing and other pre-launch decisions.
+            which can be good for validating pricing and other pre-launch
+            decisions.
           </div>
         </div>
 
         <div className="software__processStep">
           <div className="software__processLabel">
-            <div className="software__processNumber">
-              5
-            </div>
+            <div className="software__processNumber">5</div>
           </div>
           <div className="software__processInfo">
             <span className="software__processInfo--title">Launch</span>
-            In addition to being a satisfying and exciting moment, launch is also
-            important to get right. I do the advance planning to ensure a smooth
-            rollout on the app stores. I{"'"}m also happy to brainstorm on successful
-            strategies that I{"'"}ve used in the past to generate millions of dollars
-            in product revenues.
+            In addition to being a satisfying and exciting moment, launch is
+            also important to get right. I do the advance planning to ensure a
+            smooth rollout on the app stores. I{"'"}m also happy to brainstorm
+            on successful strategies that I{"'"}ve used in the past to generate
+            millions of dollars in product revenues.
           </div>
         </div>
       </div>
@@ -138,12 +133,13 @@ export default function Software() {
           className="software__aboutMePhoto"
         />
         <div className="software__aboutMeBio">
-          I{"'"}m a software developer with a design and entrepreneurial background. Before
-          I started building apps, I founded a company called Concert Window
-          which grew to over 150,000 users and generated millions of dollars in
-          revenue. I{"'"}m also a musician with four albums under my belt. I love
-          building and launching new things into the world, and these days I{"'"}m
-          using my experience in technology and startups to help build great products.
+          I{"'"}m a full stack software engineer with a design and
+          entrepreneurial background. Before I started building apps and
+          websites, I founded a company called Concert Window which grew to over
+          150,000 users and generated millions of dollars in revenue. I{"'"}m
+          also a musician with four albums under my belt. I love building and
+          launching new things into the world, and these days I{"'"}m using my
+          experience in technology and startups to help build great products.
         </div>
       </div>
 
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,59 +1,84 @@
 export const projects = [
   {
+    imageFile: 'TouchIcon.png',
+    title: 'In-Store App for Major Brand',
+    description:
+      'Working with an NYC development firm to build a React Native in-store experience',
+    descriptionLink: '',
+    appStoreLink: '',
+    playStoreLink: ''
+  },
+  {
     imageFile: 'RadioKingston.jpg',
     title: 'Radio Kingston',
-    description: 'Working with Moonfarmer to build v2 of React Native radio app',
+    description:
+      'Working with Moonfarmer to build v2 of React Native radio app',
     descriptionLink: '',
-    appStoreLink: 'https://itunes.apple.com/us/app/radio-kingston/id1351345738?mt=8',
-    playStoreLink: 'https://play.google.com/store/apps/details?id=com.radiokingston&hl=en_US',
+    appStoreLink:
+      'https://itunes.apple.com/us/app/radio-kingston/id1351345738?mt=8',
+    playStoreLink:
+      'https://play.google.com/store/apps/details?id=com.radiokingston&hl=en_US'
   },
   {
     imageFile: 'GoGladys.jpg',
     title: 'Go Gladys – Next Gen Presentations',
-    description: 'Working with the team to design and build new Meteor + React admin',
+    description:
+      'Working with the team to design and build new Meteor + React admin',
     descriptionLink: '',
     appStoreLink: '',
     playStoreLink: '',
-    webLink: 'https://www.gogladys.com/',
+    webLink: 'https://www.gogladys.com/'
   },
   {
     imageFile: 'i123.png',
     title: 'inspector123 - Mobile Tool for Inspectors',
     description: 'Read about how it was made',
-    descriptionLink: 'https://medium.com/@dgurns/building-the-living-sketchbook-app-231a1cf5252',
-    appStoreLink: 'https://itunes.apple.com/us/app/inspector123/id1269271714?ls=1&mt=8',
-    playStoreLink: 'https://play.google.com/store/apps/details?id=com.inspector123',
+    descriptionLink:
+      'https://medium.com/@dgurns/building-the-living-sketchbook-app-231a1cf5252',
+    appStoreLink:
+      'https://itunes.apple.com/us/app/inspector123/id1269271714?ls=1&mt=8',
+    playStoreLink:
+      'https://play.google.com/store/apps/details?id=com.inspector123'
   },
   {
     imageFile: 'Fed.png',
-    title: 'F\'ed - Career Re-Training for Millennials',
+    title: "F'ed - Career Re-Training for Millennials",
     description: 'See the codebase',
     descriptionLink: 'https://github.com/dgurns/MillennialJobs',
-    appStoreLink: 'https://itunes.apple.com/us/app/fed-career-re-training-for-millennials/id1246455050?ls=1&mt=8',
-    playStoreLink: 'https://play.google.com/store/apps/details?id=com.millennialjobs',
+    appStoreLink:
+      'https://itunes.apple.com/us/app/fed-career-re-training-for-millennials/id1246455050?ls=1&mt=8',
+    playStoreLink:
+      'https://play.google.com/store/apps/details?id=com.millennialjobs'
   },
   {
     imageFile: 'LivingSketchbook2.png',
     title: 'Living Sketchbook Vol. 2: Metro North',
     description: 'Read about it on the Udemy Blog',
-    descriptionLink: 'https://about.udemy.com/students/entrepreneurial-music-lover-finds-his-groove-building-mobile-apps/',
+    descriptionLink:
+      'https://about.udemy.com/students/entrepreneurial-music-lover-finds-his-groove-building-mobile-apps/',
     appStoreLink: 'https://appsto.re/us/hLg0ib.i',
-    playStoreLink: 'https://play.google.com/store/apps/details?id=com.livingsketchbook2',
+    playStoreLink:
+      'https://play.google.com/store/apps/details?id=com.livingsketchbook2'
   },
   {
     imageFile: 'LivingSketchbook1.png',
     title: 'Living Sketchbook Vol. 1: Boyhood Home',
     description: 'Read about the build process',
-    descriptionLink: 'https://medium.com/@dgurns/building-the-living-sketchbook-app-231a1cf5252',
-    appStoreLink: 'https://itunes.apple.com/us/app/living-sketchbook-vol-1-boyhood-home/id1209783393?ls=1&mt=8',
-    playStoreLink: 'https://play.google.com/store/apps/details?id=com.livingsketchbook1',
+    descriptionLink:
+      'https://medium.com/@dgurns/building-the-living-sketchbook-app-231a1cf5252',
+    appStoreLink:
+      'https://itunes.apple.com/us/app/living-sketchbook-vol-1-boyhood-home/id1209783393?ls=1&mt=8',
+    playStoreLink:
+      'https://play.google.com/store/apps/details?id=com.livingsketchbook1'
   },
   {
     imageFile: 'GurneyJourney.jpg',
     title: 'Gurney Journey',
     description: '5 stars on the App Store & Google Play',
     descriptionLink: '',
-    appStoreLink: 'https://itunes.apple.com/us/app/gurney-journey/id1179475898?ls=1&mt=8',
-    playStoreLink: 'https://play.google.com/store/apps/details?id=com.gurneyjourney',
-  },
+    appStoreLink:
+      'https://itunes.apple.com/us/app/gurney-journey/id1179475898?ls=1&mt=8',
+    playStoreLink:
+      'https://play.google.com/store/apps/details?id=com.gurneyjourney'
+  }
 ];
